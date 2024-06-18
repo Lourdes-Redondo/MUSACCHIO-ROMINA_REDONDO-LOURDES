1a6fd0fda1966f9f85ee815452723bc8827baa51
Romina Musacchio <musacchioromina@gmail.com>
2024-06-18 19:58:31 -0300

    Main change

--- src/main/java/com/backend/clinicaOdontologica/ClinicaOdontologicaApplication.java
+++ src/main/java/com/backend/clinicaOdontologica/ClinicaOdontologicaApplication.java
@@ -1,42 +1,25 @@
 package com.backend.clinicaOdontologica;
 
-import com.backend.clinicaOdontologica.dto.entrada.DomicilioEntradaDto;
-import com.backend.clinicaOdontologica.dto.entrada.OdontologoEntradaDto;
-import com.backend.clinicaOdontologica.dto.entrada.PacienteEntradaDto;
-import com.backend.clinicaOdontologica.dto.entrada.TurnoEntradaDto;
-import com.backend.clinicaOdontologica.dto.salida.TurnoSalidaDto;
-import com.backend.clinicaOdontologica.exceptions.BadRequestException;
-import com.backend.clinicaOdontologica.service.impl.TurnoService;
+
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 @SpringBootApplication
 public class ClinicaOdontologicaApplication {
 
 
-
-	public static void main(String[] args) {
-	//	SpringApplication.run(ClinicaOdontologicaApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicaOdontologicaApplication.class, args);
 
     }
 
     @Bean
-    public ModelMapper modelMapper(){
+    public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
 
-
-
 }
 
-//@bean
-//public ModelMapper modelMapper(){
-//	return new ModelMapper();
-//}
--- src/test/java/com/backend/clinicaOdontologica/ClinicaOdontologicaApplicationTests.java
+++ src/test/java/com/backend/clinicaOdontologica/ClinicaOdontologicaApplicationTests.java
@@ -2,7 +2,6 @@ package com.backend.clinicaOdontologica;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.backend.clinicaOdontologica.service.impl.OdontologoService;
 import com.backend.clinicaOdontologica.repository.impl.OdontologoDaoH2;
 import com.backend.clinicaOdontologica.repository.impl.OdontologoDaoMemoria;
 import static org.junit.jupiter.api.Assertions.*;
